aced40003dfe41dfb5ba55f4a571bb7b99e7c526
scp <cp_shen@outlook.com>
2019-11-13 20:02:10 +0800

    new mod

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
+mod load_obj;
+
+use load_obj::test;
+
 fn main() {
     println!("load mesh!");
     test();
 }
 
-fn test() {
-    use std::path::Path;
-
-    let mod_load_result = tobj::load_obj(
-        &Path::new("resources/models/african_head/african_head.obj"));
-
-    assert!(mod_load_result.is_ok());
-    let (models, materials) = mod_load_result.unwrap();
-
-    let mesh = &models[0].mesh;
-    let i = mesh.indices[0] as usize;
-    // pos = [x, y, z]
-    let pos = [mesh.positions[i * 3], mesh.positions[i * 3 + 1],
-        mesh.positions[i * 3 + 2]];
-
-    if !mesh.normals.is_empty() {
-        // normal = [x, y, z]
-        let normal = [mesh.normals[i * 3], mesh.normals[i * 3 + 1],
-            mesh.normals[i * 3 + 2]];
-    }
-
-    if !mesh.texcoords.is_empty() {
-        // texcoord = [u, v];
-        let texcoord = [mesh.texcoords[i * 2], mesh.texcoords[i * 2 + 1]];
-    }
-
-    println!("test finish!");
-}
