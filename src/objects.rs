fedd1ba808e9e6d3c12fde36a53abfa6af0c4c5f
cp_shen <cp_shen@outlook.com>
2019-11-20 14:56:35 +0800

    add load_obj for scene

--- src/objects.rs
+++ src/objects.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use cgmath::*;
+use std::path::Path;
 
 #[allow(dead_code)]
 pub struct Scene {
@@ -12,6 +13,19 @@ impl Scene {
     fn new() -> Scene {
         Scene {
             models: vec![],
+            camera: Camera::new(),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn load_obj(file_name: &Path) -> Scene {
+        let mod_load_result = tobj::load_obj(file_name);
+        assert!(mod_load_result.is_ok());
+
+        let (models, _) = mod_load_result.unwrap();
+
+        Scene {
+            models,
             camera: Camera::new(),
         }
     }
--- src/render_targets.rs
+++ src/render_targets.rs
@@ -1,14 +1,17 @@
 use crate::objects::*;
 
 trait RenderTarget {
+    #[allow(unused)]
     fn render(scene: Scene);
 }
 
+#[allow(dead_code)]
 struct ImageRenderTarget {
     //TODO
 }
 
 impl RenderTarget for ImageRenderTarget {
+    #[allow(unused)]
     fn render(scene: Scene) {
         //TODO
     }
